42403df1a3e5f16bcf6b30fbd470ce3ba46493c8
Ryan Faulkner <mf013148@reading.ac.uk>
2016-10-09 13:49:42 +0100

    v1

---
+++ SAX/src/uk/ac/reading/sse/sempr14/sax/PAARecord.java
@@ -0,0 +1,13 @@
+package uk.ac.reading.sse.sempr14.sax;
+
+public class PAARecord {
+	double start, end;
+	double level;
+	
+	PAARecord(double s, double e, double l) {
+		this.start = s;
+		this.end = e;
+		this.level = l;
+	}
+	
+}
